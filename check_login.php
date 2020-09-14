d6803b8afa431d4e4b601b0920b6acc25ab7a281
MrDarkG <vako.batiashvili1998@gmail.com>
2020-09-14 20:14:48 +0400

    login.php done

---
+++ check_login.php
@@ -0,0 +1,14 @@
+<?php 
+require_once("db.php");
+if (isset($_POST['name'],$_POST["password"])) {
+	$name=mysqli_real_escape_string($conn,$_POST["name"]);
+	$password=mysqli_real_escape_string($conn,$_POST["password"]);
+	$sql="SELECT * FROM `users` WHERE `name`='$name' AND `password`='$password'";
+	$result=$conn->query($sql);
+	if ($result->num_rows>0) {
+		echo "yeahy";
+	}
+}
+
+
+?>
--- login.php
+++ login.php
@@ -6,21 +6,17 @@
 </head>
 <body>
 	<div class="container  card">
-		
-		<form>
+		<form method="POST" action="check_login.php">
 		  <div class="form-group">
-		    <label for="exampleInputEmail1">Email address</label>
-		    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-		    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+		    <label for="exampleInputEmail1">name</label>
+		    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter name" name="name">
+		    
 		  </div>
 		  <div class="form-group">
 		    <label for="exampleInputPassword1">Password</label>
-		    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+		    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" name="password">
 		  </div>
-		  <div class="form-group form-check">
-		    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-		    <label class="form-check-label" for="exampleCheck1">Remember me</label>
-		  </div>
+		  
 		  <button type="submit" class="btn btn-primary">Login</button>
 		</form>
 	</div>
