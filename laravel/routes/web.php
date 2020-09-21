44bbc8655639b1c61226f38b299c6c9a12bbd297
MrDarkG <vako.batiashvili1998@gmail.com>
2020-09-21 20:27:00 +0400

    laravel first commit

---
+++ laravel/routes/web.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+use Illuminate\Http\Request;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get("/gettest", function() {
+	return view("guest.form");
+});
+Route::post("/test",function(Request $req){
+	$data=[
+		[
+			"name"=>$req->post()["name"]
+		],
+		[
+			"name"=>"giorgi"
+		],
+
+	];
+	// return $data;
+	return view("guest.table",["newdata"=>$data,]);
+	// return $req->post()["name"];
+})->name("testpost");
